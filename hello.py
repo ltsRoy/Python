3f5f3841d558fa1bff352aed778cadfc85f3fc7a
ltsRoy <114439860+ltsRoy@users.noreply.github.com>
2023-06-11 16:43:38 +0000

    .

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-x=2
+'''x=2
 y=3
 z=complex(x,y) 
 print(z)
@@ -7,4 +7,27 @@ print(k)
 
 lst=[7,62,72,91,72]
 print((lst[i] for i in range (len(lst))))
-#print(lst[x] : for x in range (lst)))
+#print(lst[x] : for x in range (lst)))'''
+
+a = 1 
+def f( ): 
+  a = 10
+print (a)
+
+def addEm (x,y,z): 
+  print(x + y + z)
+def prod (x,y,z): 
+  return x*y*z
+a = addEm(6,16,20) 
+b = prod(3,4,5) 
+print(a,b)
+
+def increase (x):
+  a = 0 #or any other value that you want to increase x by. 
+  a = a + x 
+  return a
+a = 20 
+b = 5 
+increase (b) 
+print (a)
+
